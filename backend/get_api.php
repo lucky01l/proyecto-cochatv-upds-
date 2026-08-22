0b1d73e6da3a6603b569b4c9af66d36db97765e6
jonathan.figueroa <jonath2291@gmail.com>
2026-08-21 21:45:12 -0400

    todos

--- backend/get_api.php
+++ backend/get_api.php
@@ -1,4 +1,28 @@
 <?php
-$url = 'https://image.tmdb.org/t/p/';
+
 $apiKey = '35c1d5d110f7f8753fcda624065e7631';
-print_r(url);
+$url = "https://api.themoviedb.org/3/movie/popular?api_key=$apiKey&language=es-ES";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "Accept: application/json",
+]);
+
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    echo "Error en cURL: " . curl_error($ch) . PHP_EOL;
+    exit;
+}
+
+curl_close($ch);
+
+$data = json_decode($response, true);
+
+echo "===============Data=================" . PHP_EOL;
+print_r($data);
+
+// O si prefieres verlo bonito y ordenado:
+// echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
---
+++ todo.md
@@ -0,0 +1,13 @@
+## Actividades Pendientes
+
+### Backend
+
+1. Crear la conexion a la API
+2. Subir las respuestas de la API al DB
+3. Crear un endpoint para que consuma el frontend
+4. Darle funcionalidades al endpoint de crud al endpoint
+
+### Fronend
+
+1. replicar lo que tenemos de la plantilla a VUE.
+2. Consumir el endpoint
